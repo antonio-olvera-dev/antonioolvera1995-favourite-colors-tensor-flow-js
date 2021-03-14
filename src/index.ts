36de39f9b88e9de8b2592b554325dff954d9ee2a
Antonio Olvera <antonioolvera1995@gmail.com>
2021-03-14 18:51:54 +0100

    Autos

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import tf from "@tensorflow/tfjs-node-gpu";
+import tfvis from '@tensorflow/tfjs-vis';
+import { Autos } from "./autos.js";
 import { Pesos } from "./pesos.js";
 import { Probes } from "./probes.js";
 import { Salary } from './salary.js'
 // new Pesos();
 // new Salary();
 // new Probes();
+// new Autos();
+
